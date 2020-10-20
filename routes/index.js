a9773447e6259952adcdb5ce97ff2c051e4a49f0
AriAlanPR <shaymin2003@gmail.com>
2020-10-20 15:27:45 -0500

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    console.log(req.query);
+
+    res.render('netsuite_tba', {
+        title: 'Netsuite TBA Authorization lo js View',
+        data: JSON.stringify(req.query)
+    });
+});
+
+router.post('/', function(req, res){
+    console.log(req.body);
+
+    //magensa.PostNetsuite(req.body);
+    res.json(req.body);
 });
 
 module.exports = router;
